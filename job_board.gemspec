f3f45e3b1515bd121f6a1b6f9ec0e91f704181c6
Nitho Alif Ibadurrahman <nitho.alif@bukalapak.com>
2017-10-26 15:53:22 +0700

    Add CRUD operations

--- job_board.gemspec
+++ job_board.gemspec
@@ -31,9 +31,10 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.16.a"
-  spec.add_development_dependency "dotenv", "~> 2.2.1"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   
+  spec.add_runtime_dependency "dotenv", "~> 2.2.1"
+  spec.add_runtime_dependency "json", "~> 2.1.0"
   spec.add_runtime_dependency "mysql2", "~> 0.4.9"
 end
--- lib/job_board.rb
+++ lib/job_board.rb
@@ -1,5 +1,6 @@
-require 'mysql2'
 require 'dotenv'
+require 'mysql2'
+require 'json'
 
 require "job_board/version"
 
@@ -16,7 +17,7 @@ module JobBoard
     @mysql_port = port || ENV['MYSQL_PORT']
     @mysql_user = user || ENV['MYSQL_USER']
     @mysql_pass = pass || ENV['MYSQL_PASS']
-    @mysql_db = db || ENV['MYSQL_db']
+    @mysql_db = db || ENV['MYSQL_DB']
 
     # Connect to database
     begin
@@ -29,8 +30,10 @@ module JobBoard
         )
     rescue
       puts "Could not connect to database, please check the settings"
+      return 1
     else
       puts "Connection successful"
+      return 0
     end
   end
 
@@ -52,5 +55,97 @@ module JobBoard
     puts @mysql_user
     puts @mysql_pass
     puts @mysql_db  
+  end
+
+  def self.create(category, position, company, post, email)
+    begin
+      statement = @db.prepare(
+        "INSERT INTO jobs (category, position, company, post, email)
+        VALUES (?, ?, ?, ?, ?)"
+        )
+      result = statement.execute(
+        category,
+        position,
+        company,
+        post,
+        email
+        )
+    rescue
+      puts "Could not create new listing, please check the database connection"
+      return 1
+    else
+      puts "Job succesfully listed"
+      return 0
+    end
+  end
+
+  def self.get_all
+    begin
+      result = @db.query("SELECT * FROM jobs")
+    rescue
+      puts "Could not get job listing, please check the database connection"
+      return 1
+    else
+      return result
+    end
+  end
+
+  def self.get(id)
+    begin
+      statement = @db.prepare("SELECT * FROM jobs WHERE id = ?")
+      result = statement.execute(id)
+    rescue
+      puts "Could not get job listing, please check the database connection"
+      return 1
+    else
+      result.each do |row|
+        return row
+      end
+    end
+  end
+
+  def self.update(id, category, position, company, post, email)
+    begin
+      statement = @db.prepare(
+        "UPDATE jobs
+        SET category=?, position=?, company=?, post=?, email=?
+        WHERE id = ?")
+      result = statement.execute(
+        category,
+        position,
+        company,
+        post,
+        email,
+        id)
+
+      if statement.affected_rows != 1
+        puts "Could not find job listing with id = ".concat(id.to_s)
+        return 2
+      end
+    rescue 
+      puts "Could not update job listing, please check the database connection"
+      return 1
+    else
+      puts "Job succesfully updated"
+      return 0
+    end
+  end
+
+  def self.delete(id)
+    begin
+      statement = @db.prepare("DELETE FROM jobs WHERE id = ?")
+      result = statement.execute(id)
+
+      if statement.affected_rows != 1
+        puts "Could not find job listing with id = ".concat(id.to_s)
+        return 2
+      end
+    rescue 
+      puts "Could not delete job listing, please check the database connection"
+      return 1
+    else
+      puts "Job succesfully deleted"
+      return 0
+    end
   end
 end
