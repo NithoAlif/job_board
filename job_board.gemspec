70fa8dd625ab8b419b55b3ecea90a9ad98b90b3a
Nitho Alif Ibadurrahman <nitho.alif@bukalapak.com>
2017-10-26 14:35:04 +0700

    Add database settings

--- job_board.gemspec
+++ job_board.gemspec
@@ -9,13 +9,13 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Nitho Alif Ibadurrahman"]
   spec.email         = ["nitho.alif@bukalapak.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = "Simple Job Board wrapper"
+  spec.homepage      = 'https://www.bukalapak.com'  
   spec.license       = "MIT"
 
-  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
-  # to allow pushing to a single host or delete this section to allow pushing to any host.
+  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the
+  # 'allowed_push_host' to allow pushing to a single host or delete this section
+  # to allow pushing to any host.
   if spec.respond_to?(:metadata)
     spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
   else
@@ -31,6 +31,9 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.16.a"
+  spec.add_development_dependency "dotenv", "~> 2.2.1"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  
+  spec.add_runtime_dependency "mysql2", "~> 0.4.9"
 end
--- lib/job_board.rb
+++ lib/job_board.rb
@@ -1,5 +1,56 @@
+require 'mysql2'
+require 'dotenv'
+
 require "job_board/version"
 
 module JobBoard
-  # Your code goes here...
+  class << self
+    attr_accessor :mysql_host, :mysql_port, :mysql_user, :mysql_pass, :mysql_db    
+    attr_accessor :db
+  end
+
+  def self.configure(host=nil, port=nil, user=nil, pass=nil, db=nil)
+    # Load .env, system/user set environment take precedence
+    Dotenv.load('.env')
+    @mysql_host = host || ENV['MYSQL_HOST']
+    @mysql_port = port || ENV['MYSQL_PORT']
+    @mysql_user = user || ENV['MYSQL_USER']
+    @mysql_pass = pass || ENV['MYSQL_PASS']
+    @mysql_db = db || ENV['MYSQL_db']
+
+    # Connect to database
+    begin
+      @db = Mysql2::Client.new(
+        :host => @mysql_host,
+        :port => @mysql_port,
+        :username => @mysql_user,
+        :password => @mysql_pass,
+        :database => @mysql_db
+        )
+    rescue
+      puts "Could not connect to database, please check the settings"
+    else
+      puts "Connection successful"
+    end
+  end
+
+  def self.reset_configuration
+    # Close database connection
+    @db.close
+
+    # Reset configuration value
+    @mysql_host = nil
+    @mysql_port = nil
+    @mysql_user = nil
+    @mysql_pass = nil
+    @mysql_db = nil
+  end
+
+  def self.print_configuration
+    puts @mysql_host
+    puts @mysql_port
+    puts @mysql_user
+    puts @mysql_pass
+    puts @mysql_db  
+  end
 end
